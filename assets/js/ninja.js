666b26264f68d1491df17fa9038aa729ae34b6cf
Matthew Bunday <zenceph@gmail.com>
2014-09-21 14:57:14 -0400

    Basic game object work

--- assets/js/ninja.js
+++ assets/js/ninja.js
@@ -1,5 +1,11 @@
 Game = {
+  arena: function() {
+    return $('#arena');
+  },
   loop: function() {
     console.log("Hello");
+  },
+  init: function() {
+    this.arena().append("<div id='ninja'></div>");
   }
 }
