08446bc9e803c18af7e55940ffe253ea3f1a2700
Matthew Bunday <zenceph@gmail.com>
2014-09-21 15:20:26 -0400

    Refactored to use object classes

--- assets/js/ninja.js
+++ assets/js/ninja.js
@@ -1,11 +1,19 @@
-Game = {
-  arena: function() {
-    return $('#arena');
-  },
-  loop: function() {
-    console.log("Hello");
-  },
-  init: function() {
-    this.arena().append("<div id='ninja'></div>");
-  }
+function Game() {
+  this.$arena = $('#arena');
+  this.ninja = new Ninja(this.$arena);
 }
+
+function Ninja($arena) {
+  this.x = 400;
+  this.y = 400;
+  this.$arena = $arena;
+  this.init();
+}
+
+Ninja.prototype.init = function() {
+  this.$arena.append("<div id='ninja'></div>");
+}
+
+$(document).ready(function() {
+  game = new Game();
+});
