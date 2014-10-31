6b5c0cc1a6c42a6ebf31a4c956b75489740cfe0e
Matthew Bunday <zenceph@gmail.com>
2014-10-31 10:25:06 -0400

    Starting out a ninja class.

--- assets/js/ninja.js
+++ assets/js/ninja.js
@@ -1,5 +1,24 @@
+function Ninja() {
+  this.x = 300;
+  this.y = 300;
+  this.updateDisplay = function() {
+    console.log(this.x);
+    console.log(this.y);
+  }
+}
+
+Ninja.prototype.updateDisplay = function() {
+  console.log(this.x);
+  console.log(this.y);
+}
+
+Ninja.classMethod = function() {
+
+}
+
 Game = {
   loop: function() {
     console.log("Hello");
+    return "goodbye";
   }
 }
